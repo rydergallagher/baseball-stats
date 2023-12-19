ee423a64867cc033ea1c30d3ebfab5d48f6f5349
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 15:48:44 -0800

    rax is not being returned properly

--- calculate_ba.s
+++ calculate_ba.s
@@ -3,4 +3,13 @@
 
 calculate_ba:
 
-retq
+    movq %rdi, %rax  # moves argument 1 (hits) into rax (return value, as well as result for division)
+    movq %rsi, %r9   # moves argument 2 (plate appearances) into r9 (for organizational purposes)
+    movq $0, %rdx    # moves 0 into rdx (remainder value for division)
+
+    divq %rsi       # rax = rax / rsi (hits / plate appearances)
+
+
+
+    retq
+    
--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-int calculate_ba(int plate_appearances, int hits); //function to calculate batting average
+float calculate_ba(int plate_appearances, int hits); //function to calculate batting average
 
 int main(){
 
     int pa;   // number of plate appearances
     int hits; // number of hits
-    int ba;   // batting average
+    float ba;   // batting average
 
     printf("Enter number of Plate Appearances: \n");
-    scanf("%d", &pa);
+    //scanf("%d", &pa);
+    pa = 100;
 
-    printf("Enter number of hits");
-    scanf("%d", &hits);
+    printf("Enter number of hits: \n");
+    //scanf("%d", &hits);
+    hits = 20;
 
-    ba = calculate_ba(pa, hits);
+    ba = calculate_ba(hits, pa);
+    printf("%f", ba);
 
     return 0;
 }
