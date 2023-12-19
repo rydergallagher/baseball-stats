2e14a6afd93ee64b89e010b3da17d1676a85b221
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 15:00:16 -0800

    added batting average, and initialized function to calculate it

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+int calculate_ba(int plate_appearances, int hits); //function to calculate batting average
+
 int main(){
 
     int pa;   // number of plate appearances
     int hits; // number of hits
+    int ba;   // batting average
 
     printf("Enter number of Plate Appearances: \n");
     scanf("%d", &pa);
 
     printf("Enter number of hits");
     scanf("%d", &hits);
+
+    ba = calculate_ba(pa, hits);
 
     return 0;
 }
