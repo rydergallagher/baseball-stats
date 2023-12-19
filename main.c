feebd7e83235f3e072863bc7c9e6b9eb31f8df9e
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 14:53:01 -0800

    added hits to main.c

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 int main(){
 
     int pa;   // number of plate appearances
-
+    int hits; // number of hits
 
     printf("Enter number of Plate Appearances: \n");
     scanf("%d", &pa);
+
+    printf("Enter number of hits");
+    scanf("%d", &hits);
 
     return 0;
 }
