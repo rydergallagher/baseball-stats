e657df02f985cd7d7a08bd991364d23aca68a4cf
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 19:04:54 -0800

    Fixed the issue, just a stupid $ in the place of %

--- calculate_obp.s
+++ calculate_obp.s
@@ -5,7 +5,7 @@ calculate_obp:
     # 0 = pa, 1 = hits, 2 = walks, 3 = hbp, 4 = sacflies
 
     vmovaps %xmm0, %xmm8            # move arg 1 into xmm8 (for organization)
-    vxorps %xmm0, %xmm0 , %xmm0     # 0 out return register
+    vxorps %xmm0, %xmm0, %xmm0     # 0 out return register
     vaddss %xmm1, %xmm2, %xmm9      # xmm9 = xmm1 + xmm2 (hits + walks)
     vaddss %xmm3, %xmm9, %xmm9      # xmm9 = xmm9 (hits + walks) + xmm3 (hbp)
 
@@ -15,6 +15,6 @@ calculate_obp:
 
     # vdivss src1, src2, dst
     # dst = srcs2 / srcs1
-    vdivss %xmm10, $xmm9, %xmm0     # xmm0 (return)= xmm9 / xmm10
+    vdivss %xmm10, %xmm9, %xmm0     # xmm0 (return)= xmm9 / xmm10
 
     retq
