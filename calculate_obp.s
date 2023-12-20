7a7437986717ea86e8f48a02027f309be800ad14
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 17:47:12 -0800

    added new variabbles to main + added calculate_obp function

---
+++ calculate_obp.s
@@ -0,0 +1,6 @@
+    .global calculate_obp
+    .text
+
+calculate_obp:
+
+    retq
--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 
 float calculate_ba(float plate_appearances, float hits); //function to calculate batting average
 
+float calculate_obp(float plate_appearances, float hits, float walks, float hitbypitches, float sacflies);
+
 int main(){
 
     float pa;   // number of plate appearances
     float hits; // number of hits
+    float walks; // number of walks
+    float hbp; // number of hit by pitches
+    float sflies; // number of sacrifice flies
     float ba; // batting average
 
     printf("Enter number of Plate Appearances: \n");
@@ -16,6 +21,18 @@ int main(){
     scanf("%f", &hits);
     //hits = 20; // for testing
 
+    printf("Enter number of walks: \n");
+    scanf("%f", &walks);
+    //walks = 10 // for testing
+
+    printf("Enter number of hit by pitches: \n");
+    scanf("%f", &hbp);
+    //hbp = 5 // for testing
+
+    printf("Enter number of sacrifice flies: \n");
+    scanf("%f", &sflies);
+    //sflies = 10 // for testing
+    
     ba = calculate_ba(hits, pa);
     printf("The batting average is: \n");
     printf("%.3f", ba);
