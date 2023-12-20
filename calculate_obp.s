230dd766d0e5a1f046b6df7bf5b86d7bdf31068b
rydergallagher <rgallagher03@yahoo.com>
2023-12-19 18:50:13 -0800

    wrote calculate_obp function

--- calculate_obp.s
+++ calculate_obp.s
@@ -2,5 +2,19 @@
     .text
 
 calculate_obp:
+    # 0 = pa, 1 = hits, 2 = walks, 3 = hbp, 4 = sacflies
+
+    vmovaps %xmm0, %xmm8            # move arg 1 into xmm8 (for organization)
+    vxorps %xmm0, %xmm0 , %xmm0     # 0 out return register
+    vaddss %xmm1, %xmm2, %xmm9      # xmm9 = xmm1 + xmm2 (hits + walks)
+    vaddss %xmm3, %xmm9, %xmm9      # xmm9 = xmm9 (hits + walks) + xmm3 (hbp)
+
+    vaddss %xmm8, %xmm2, %xmm10     # xmm10 = xmm8 + xmm2 (plate appearances + walks)
+    vaddss %xmm3, %xmm10, %xmm10    # xmm10 = xmm10 + xmm3 (plate appearances + walks + hit by pitches)
+    vaddss %xmm4, %xmm10, %xmm10    # xmm10 = xmm10 + xmm4 (plate appearances + walks + hy by pitches + sac flies)
+
+    # vdivss src1, src2, dst
+    # dst = srcs2 / srcs1
+    vdivss %xmm10, $xmm9, %xmm0     # xmm0 (return)= xmm9 / xmm10
 
     retq
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-float calculate_ba(float plate_appearances, float hits); //function to calculate batting average
+float calculate_ba(float hits, float plate_appearances); // function to calculate batting average
 
-float calculate_obp(float plate_appearances, float hits, float walks, float hitbypitches, float sacflies);
+float calculate_obp(float plate_appearances, float hits, float walks, float hitbypitches, float sacflies); // function to calculate on base percentage
 
 int main(){
 
@@ -12,6 +12,7 @@ int main(){
     float hbp; // number of hit by pitches
     float sflies; // number of sacrifice flies
     float ba; // batting average
+    float obp; // on base percentage
 
     printf("Enter number of Plate Appearances: \n");
     scanf("%f", &pa);
@@ -32,11 +33,16 @@ int main(){
     printf("Enter number of sacrifice flies: \n");
     scanf("%f", &sflies);
     //sflies = 10 // for testing
-    
+
     ba = calculate_ba(hits, pa);
     printf("The batting average is: \n");
     printf("%.3f", ba);
     printf("\n");
 
+    obp = calculate_obp(pa, hits, walks, hbp, sflies);
+    printf("The on base percentage is: \n");
+    printf("%.3f", obp);
+    printf("\n");
+    
     return 0;
 }
